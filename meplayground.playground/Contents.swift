77d879bb4460b5915ab62f00010854cfff29ad37
MM <apple@Apples-MacBook-Pro-2.local>
2019-07-08 14:54:19 -0700

    Cool stuff

---
+++ meplayground.playground/Contents.swift
@@ -0,0 +1,102 @@
+import UIKit
+
+var str = "Hello, playground"
+var age = 17
+"i am \(age) years old"
+
+let string1 = "hello"
+let string2 = "there"
+var welcome = "\(string1) \(string2)"
+var anotherwelcome = string1 + " " + string2
+ print("Hello, World!")
+
+var first = "Karlie"
+var last = "Kloss"
+print("\(first)" + " " + "\(last)")
+print("\(last)" + " " + "\(first)")
+print("\(last)" + " " + "\(first)" + " " + "\(last)" + " " + "\(first)")
+print("I love \(first)")
+
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+var e = ((a+b+c+d)/4)
+print (Double(e))
+print (a + b * c / d)
+print (a * b)
+
+let me = "Angelina"
+let address = "11123 woah street"
+let birthday = "03/03/2002"
+var gradelvl = "12"
+var year = 17
+let eyecolor = "brown"
+let favcolor = "green"
+print ("\(me)" + "\(address)" + "\(birthday)" + "\(gradelvl)" + "\(year)" + "\(eyecolor)" + "\(favcolor)")
+
+var w = 10.25
+var h = 20
+print (Double(h) * w)
+
+let woah = "Beyonce"
+print ("Happy Birthday \(woah)")
+
+var name = "Klossy"
+print("\(name.first!)\(name.count-2)\(name.last!)")
+
+5 < 3
+12 > 7
+6 != 8
+7 == 7
+
+"karlie" == "karlie"
+"karlie" == " karlie"
+"Karlie" == "karlie"
+
+var luckyNum = 7
+luckyNum < 10
+luckyNum == 7
+
+
+var angie = 2
+if angie < 1 {
+    print("angie is great")
+}
+else if angie == 0 {
+    print("IMPOSTER")
+}
+else {
+    print("oh no")
+}
+
+var favoritefood = "Chipotle"
+if favoritefood == "Chipotle" {
+    print("no, that's gross")
+}
+else if favoritefood == "Starbucks" {
+    print("es expensive")
+}
+else {
+    print("French fries is my favorite food")
+}
+
+var hasaccount = true
+if hasaccount == false {
+    print("Please Sign up Here!")
+}
+else {
+    print("Login in here!")
+}
+
+var grad = 10
+if grad <= 5 {
+    print("You're a baby. You're still in elementary")
+}
+else if grad > 8 {
+    print("gEt rEAdy fOr cOlLegE aPps")
+}
+else {
+    print("pUbErTy")
+}
+
